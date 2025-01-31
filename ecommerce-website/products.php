dd02f6864487ed092f3a10ccf219eb10c807ccff
Rrivald <rivaldoaldo756@gmail.com>
2025-01-31 21:11:43 +0700

    Add files via upload

---
+++ ecommerce-website/products.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+    <!-- Include Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div data-aos="fade-up" data-aos-duration="1000" style="text-align: center; color: white; margin-top: 150px;">
+        <h1>Upcoming Products!!</h1>
+        <a href="http://localhost/ecommerce-website" class="btn btn-primary" style="opacity: 0.8;">click to go back</a>
+    </div>
+</body>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+</html>
+
